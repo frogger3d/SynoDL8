3a6a68a825fbbce98cbc7515442e56b039673cfd
Wouter de Vries <w.l.devries@gmail.com>
2013-04-22 23:48:40 +0200

    yay we got an ajax return from the diskstation

--- SynoDL8/ItemsPage.xaml.cs
+++ SynoDL8/ItemsPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using SynoDL8.Data;
-
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics.Display;
@@ -26,9 +27,44 @@ namespace SynoDL8
     /// </summary>
     public sealed partial class ItemsPage : SynoDL8.Common.LayoutAwarePage
     {
+        const string host = @"http://diskstation:5000/";
+        const string infoquery = @"webapi/query.cgi?api=SYNO.API.Info&version=1&method=query&query=SYNO.API.Auth,SYNO.DownloadStation.Task";
+        /// <summary>
+        /// auth query. string format {0} = account, string format {1} is passwd
+        /// </summary>
+        const string authquery = @"webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account={0}&passwd={1}&session=DownloadStation&format=cookie";
+
+
         public ItemsPage()
         {
             this.InitializeComponent();
+
+            var client = new System.Net.Http.HttpClient();
+
+            // http://myds.com/webapi/query.cgi?api=SYNO.API.Info&version=1&method=query&query=SYNO.API.Auth,SYNO.DownloadStation.Task
+            // http://myds.com/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=admin&passwd=12345&session=DownloadStation&format=cookie
+            // http://myds.com/webapi/DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=list
+
+            HttpRequestMessage oAuthRequestMessage = new HttpRequestMessage();
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(host + infoquery);
+
+            request.BeginGetResponse(new AsyncCallback(ReadWebRequestCallback), request); 
+        }
+
+        // STEP4 STEP4 STEP4
+        private void ReadWebRequestCallback(IAsyncResult callbackResult)
+        {
+            HttpWebRequest myRequest = (HttpWebRequest)callbackResult.AsyncState;
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.EndGetResponse(callbackResult);
+
+            using (StreamReader httpwebStreamReader = new StreamReader(myResponse.GetResponseStream()))
+            {
+                string results = httpwebStreamReader.ReadToEnd();
+                //TextBlockResults.Text = results; //-- on another thread!
+                Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => this.Content = new TextBlock() { Text = results });
+            }
+            myResponse.Dispose();
         }
 
         /// <summary>
