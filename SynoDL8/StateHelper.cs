e2d71c2b07c257c51383c3624447ae1ab64b0c4f
Wouter de Vries <w.l.devries@gmail.com>
2014-05-03 19:20:32 +0200

    synology methods now (for the most part) return SynologyResponse to better handle errors. Exceptions and reactive programming are lost to me (for now..)

---
+++ SynoDL8/StateHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SynoDL8
+{
+    public class StateHelper : DependencyObject
+    {
+        public static readonly DependencyProperty StateProperty = DependencyProperty.RegisterAttached(
+            "State", typeof(String), typeof(StateHelper), new PropertyMetadata("Normal", StateChanged));
+
+        internal static void StateChanged(DependencyObject target, DependencyPropertyChangedEventArgs args)
+        {
+            if (args.NewValue != null)
+            {
+                VisualStateManager.GoToState((Control)target, (string)args.NewValue, true);
+            }
+        }
+
+        public static void SetState(DependencyObject obj, string value)
+        {
+            obj.SetValue(StateProperty, value);
+        }
+
+        public static string GetState(DependencyObject obj)
+        {
+            return (string)obj.GetValue(StateProperty);
+        }
+    }
+}
