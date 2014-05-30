6241c77d7fdbd82e129e513cc1da661b6c26bbd8
Wouter de Vries <w.l.devries@gmail.com>
2014-05-31 01:02:31 +0200

    LoginPage is now layout aware

--- SynoDL8/Views/LoginPage.xaml.cs
+++ SynoDL8/Views/LoginPage.xaml.cs
@@ -23,7 +23,7 @@ namespace SynoDL8.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class LoginPage : Page
+    public sealed partial class LoginPage : VisualStateAwarePage
     {
         public LoginPage()
         {
