cafe7e72bb6afc435d172a74027bbfc7d87b8055
Wouter de Vries <w.l.devries@gmail.com>
2014-05-30 23:21:13 +0200

    added design viewmodel

---
+++ SynoDL8/DesignViewModels/MainDesignViewModel.cs
@@ -0,0 +1,47 @@
+﻿namespace SynoDL8.DesignViewModels
+{
+    using ReactiveUI;
+    using ReactiveUI.Xaml;
+    using SynoDL8.Model;
+    using SynoDL8.Services;
+    using SynoDL8.ViewModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Reactive;
+    using System.Reactive.Linq;
+    using System.Reactive.Subjects;
+    using System.Reactive.Threading.Tasks;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using Windows.UI.Popups;
+    using Windows.UI.Xaml;
+
+    public class MainDesignViewModel : IMainViewModel
+    {
+        public MainDesignViewModel()
+        {
+            this.UploadSpeed = "Upload 5 KBps";
+            this.DownloadSpeed = "Download 50 KBps";
+        }
+        public ReactiveCommand VersionsCommand { get; set; }
+
+        public ReactiveCommand InfoCommand { get; set; }
+
+        public ReactiveCommand ListCommand { get; set; }
+
+        public ReactiveCommand CreateCommand { get; set; }
+
+        public string Url { get; set; }
+
+        public string Message { get; set; }
+
+        public ReactiveList<DownloadTaskViewModel> Content { get; set; }
+
+        public string UploadSpeed { get; set; }
+
+        public string DownloadSpeed { get; set; }
+    }
+}
